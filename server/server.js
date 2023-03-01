15d41c78ec1a3b334db83f5faf0927f727e2292f
JairoProDev <JairoProDev@gmail.com>
2023-03-01 13:35:46 -0500

    uploading-to-github

--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
-//importando modulos
+// Importando módulos
 const express = require('express');
-const app = express();
-const fs = require('fs');
-const multer = require('multer');
-const cors = require('cors');
-const path = require('path');
+const fs = require('fs'); // Módulo para leer archivos del sistema de archivos
+const multer = require('multer'); // Módulo para gestionar la subida de archivos
+const cors = require('cors'); // Módulo para habilitar CORS en Express
+const path = require('path'); // Módulo para trabajar con rutas de archivos
 
-const dotenv = require('dotenv')
+//creando instancia de la aplicación express
+const app = express();
+
+//cargar la configuración de las variables de entorno
+const dotenv = require('dotenv') // Módulo para cargar variables de entorno desde un archivo .env
 dotenv.config()
 
-
-
+// Habilitar CORS en la app de Express para permitir el acceso desde cualquier origen.
 app.use(cors())
 
-
+// Módulo para servir archivos estáticos desde el directorio 'views/navbar-menu'
 const serveStatic = require('serve-static');
 app.use(serveStatic('views/navbar-menu'));
 
+// Configuración de multer para guardar los archivos subidos en el directorio 'uploads' y renombrarlos con el timestamp actual y el nombre original del archivo
 const storage = multer.diskStorage({
     destination: __dirname + '/uploads',
     filename: function(req, file, cb){
@@ -25,24 +28,26 @@ const storage = multer.diskStorage({
     }
 })
 
+// Configuración de multer para gestionar la subida de archivos
 const upload =multer({
     //dest: 'uploads/'
     storage: storage,
 })
- app.get('/', (req, res) => {
-     res.sendFile(__dirname +"/views/navbar-menu/index.html");
- });
 
-app.use(express.static(path.join(__dirname, 'views')));
-/*
+// Ruta principal de la aplicación, sirve el archivo index.html desde el directorio 'views/navbar-menu'
 app.get('/', (req, res) => {
-    res.sendFile(__dirname +"/navbar-menu/index.html");
+    res.sendFile(__dirname +"/views/navbar-menu/index.html");
 });
-*/
+
+// Middleware para servir archivos estáticos desde el directorio 'views'
+app.use(express.static(path.join(__dirname, 'views')));
+
+// Ruta para gestionar la subida de archivos
 app.post("/archivos", upload.single('clase'), (req, res) => {
     res.send("funcionó, el archivo fue subido")
 })
 
+// Ruta para obtener la lista de archivos subidos
 app.get('/archivos', (req, res) => {
     fs.readdir(__dirname + '/uploads', (err, files) => {
         if (err) {
@@ -53,13 +58,10 @@ app.get('/archivos', (req, res) => {
     });
 });
 
-
-
-
+// La app de Express escucha en el puerto 8080
 app.listen(8080, () => console.log('listening on port 8080'));
 
 
 
 // const connectDB = require('./config/db');
-
 // connectDB();
